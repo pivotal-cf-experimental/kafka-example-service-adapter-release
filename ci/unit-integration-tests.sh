e7eb005c657af041a35cbb2a3efeee73ae12430c
Jonathan Tsang <jtsang@pivotal.io>
2017-11-09 11:10:26 +0000

    Remove godep restore - now using dep
    [#151549249]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- ci/unit-integration-tests.sh
+++ ci/unit-integration-tests.sh
@@ -5,9 +5,6 @@ pushd $(dirname $0)/..
   export PATH=$GOPATH/bin:$PATH
 
   pushd src/github.com/pivotal-cf-experimental/kafka-example-service-adapter
-    go get -v github.com/tools/godep
-    godep restore
-
     go install -v github.com/onsi/ginkgo/ginkgo
 
     ginkgo -r -randomizeAllSpecs -randomizeSuites -race -keepGoing -failOnPending -cover
