a6f1c5d3d2d034e86ffaf60b95fe9d99155fff44
Andrew Garner <garnera@vmware.com>
2024-02-06 15:11:47 -0600

    ci: use vendored ginkgo cli
    This aligns with how ginkgo is run in other ODB components.
    
    Note this is still ginkgo v1 which has not been updated in these example
    releases.

--- ci/unit-integration-tests.sh
+++ ci/unit-integration-tests.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -eu
 
 pushd $(dirname $0)/..
-  export GOPATH=$PWD
-  export PATH=$GOPATH/bin:$PATH
+export GOPATH=$PWD
+export PATH=$GOPATH/bin:$PATH
 
-  pushd src/github.com/pivotal-cf-experimental/kafka-example-service-adapter
-    go get -u github.com/onsi/ginkgo/ginkgo
-
-    GO111MODULE=off ginkgo -r -randomizeAllSpecs -randomizeSuites -race -keepGoing -failOnPending -cover
-  popd
+pushd src/github.com/pivotal-cf-experimental/kafka-example-service-adapter
+go run github.com/onsi/ginkgo/ginkgo -r -randomizeAllSpecs -randomizeSuites -race -keepGoing -failOnPending -cover
+popd
 popd
